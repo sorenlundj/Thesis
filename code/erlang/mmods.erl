c96061292abd76a0327ebeaed9e4d882ab902dbe
sorenlundj <Sorenlundj@gmail.com>
2020-01-24 21:44:08 +0100

    Added functionality
    + add_relation
    + add_dependency
    + get_state
    + get_type
    + get_ relations

--- code/erlang/mmods.erl
+++ code/erlang/mmods.erl
@@ -2,7 +2,7 @@
 
 -behaviour(gen_statem).
 
--export([start/0, add_relation/2, get_state/1]).
+-export([start/1, add_relation/2, get_state/1, get_type/1, get_relations/1, add_dependency/3]).
 
 %-export([init/1, callback_mode/0, terminate/3, code_change/4]).
 -export([init/1, callback_mode/0, terminate/3, code_change/4, mmods_handler/3]).
@@ -13,24 +13,36 @@
 % Types                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--type name() :: pid().
+%-type name() :: pid().
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % API                                                               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--spec start() -> {ok, name() | error, any()}.
-start() ->
-  gen_statem:start_link(?MODULE, [], []).
+%-spec start(Type) -> {ok, name() | error, any()}.
+start(Type) ->
+  case Type of
+    ship    -> gen_statem:start_link(?MODULE, Type, []);
+    service -> gen_statem:start_link(?MODULE, Type, []);
+    company -> gen_statem:start_link(?MODULE, Type, []);
+    _       -> {error, not_an_entity}
+  end.
 
 add_relation(From, To) ->
-	gen_statem:call(From, {add_relation_call, To}).
+  gen_statem:call(From, {add_relation_call, To}).
 
-%add_dependency(From, To, Dependency) ->
-%	gen_statem:call(From, {add_dependency_call, To, Dependency}).
+add_dependency(From, To, Dependency) ->
+ gen_statem:call(From, {add_dependency_call, {To, Dependency}}).
 
 get_state(Id) ->
-	gen_statem:call(Id, {get_state_call, none}).
+  gen_statem:call(Id, {get_state_call, none}).
+
+get_type(Id) ->
+  gen_statem:call(Id, {get_type_call, none}).
+
+get_relations(Id) ->
+  gen_statem:call(Id, {get_relations_call, none}).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Callback functions                                                %
@@ -54,28 +66,42 @@ code_change(_Vsn, State, Data, _Extra) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %mmods_handler(cast, {Function_name, Request}, {Type, Relations}=State) ->
-%	case Function_name of
-%		add_relation_call ->
-%			{To, Dependency} = Request, 
+% case Function_name of
+%   add_relation_call ->
+%     {To, Dependency} = Request, 
 %
 %
-%	end.
+% end.
 
 
 mmods_handler({call, From}, {Function_name, Request}, {Type, Relations, Self}=State) ->
-	case Function_name of
-		add_relation_call -> %check if type matches here
-			To = Request, 
-			{keep_state, {Type, [To|Relations], Self}, [{reply, From, {ok, Self}}]};
-%		add_dependency_call -> % Check if relation exists here
-%			{keep_state, {Type, }}
-%		get_state_call ->
-%			{keep_state, State, [{reply, From, State}]};
-		get_state_call ->
+  case Function_name of
+    add_relation_call ->
+      To = Request,
+      case legal_relation(Type, get_type(To)) of
+        true  -> 
+          {keep_state, {Type, [{To, []}|Relations], Self}, [{reply, From, {ok, Self}}]};
+        false ->
+          {keep_state, State, [{reply, From, {error, illegal_relation}}]}
+      end;
+    add_dependency_call ->
+      {To, Dependency} = Request,
+      case relation_exists(To, Relations) of
+        true  ->
+          Updated_relations = dep_to_rel_list(Dependency, Relations),
+          {keep_state, {Type, Updated_relations, Self}, [{reply, From, {ok, Dependency}}]};
+        false ->
+          {keep_state, State, [{reply, From, {error, relation_doesnt_exist}}]}
+      end;
+    get_state_call ->
       {keep_state, State, [{reply, From, State}]};
-		_ ->
-			{keep_state, State, [{reply, From, {error, unhandled_case}}]}
-		end.
+    get_type_call ->
+      {keep_state, State, [{reply, From, Type}]};
+    get_relations_call ->
+      {keep_state, State, [{reply, From, Relations}]};
+    _ ->
+      {keep_state, State, [{reply, From, {error, unhandled_case}}]}
+    end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Non-op-level functions                                            %
@@ -84,3 +110,38 @@ mmods_handler({call, From}, {Function_name, Request}, {Type, Relations, Self}=St
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Auxiliary functions                                               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+legal_relation(From, To) ->
+  case {From, To} of
+    {ship, company} -> false;
+    {company, ship} -> false;
+    _               -> true
+  end.
+
+relation_exists(_, []) ->
+  false;
+relation_exists(From, [{R, _}|Relations]) ->
+  case From == R of
+    true  -> true;
+    false -> relation_exists(From, Relations)
+  end.
+
+dep_to_rel_list(Dep, [{Rel_head, Dep_list}|Rel_list]) ->
+  [{Rel_head, [Dep|Dep_list]}|Rel_list].
+  
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TODOS
+% felt i state 'data' (her ligger kortet)
+% funktion 'transfer data', der tjekker om der er en relation, og så gør den bare
+% funktion 'request' (ship->service->company), der ser om der er en vej fra ship til company, og så reqer data
+
+% på linje 94 er det Relations, der skal bruges (og ikke get_relations(to))
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
--- code/erlang/tests.erl
+++ code/erlang/tests.erl
@@ -1,16 +1,29 @@
 -module(tests).
 
--import(mmods, [start/0]).
+-import(mmods, [start/1, add_relation/2, get_state/1, get_type/1, get_relations/1, add_dependency/3]).
 
 -compile(export_all).
 -compile(nowarn_export_all).
 
 main() ->
-	{ok, Ship}    = mmods:start(),
-	{ok, Service} = mmods:start(),
-	{ok, Company} = mmods:start(),
-	{ok, Ship}    = mmods:add_relation(Ship, Service),
-	A = mmods:get_state(Ship),
-	B = mmods:get_state(Service),
-	C = mmods:get_state(Company),
-	{A, B, C}.
+  {ok, Ship}    = mmods:start(ship),
+  {ok, Service} = mmods:start(service),
+  {ok, Company} = mmods:start(company),
+  {ok, Ship}    = mmods:add_relation(Ship, Service),
+  {ok, Service} = mmods:add_relation(Service, Company),
+  {ok, Service} = mmods:add_relation(Service, Ship),
+  {ok, Company} = mmods:add_relation(Company, Service),
+  {ok, test1}   = mmods:add_dependency(Ship, Service, test1),
+  {ok, test2}   = mmods:add_dependency(Ship, Service, test2),
+  {ok, test3}   = mmods:add_dependency(Ship, Service, test3),
+  A             = mmods:get_state(Ship),
+  B             = mmods:get_state(Service),
+  C             = mmods:get_state(Company),
+  {A, B, C}.
+
+t2() ->
+  {ok, Ship1} = mmods:start(ship),
+  {ok, Ship2} = mmods:start(ship),
+  A = mmods:get_state(Ship1),
+  B = mmods:get_state(Ship2),
+  {A, B}.

8d315f6e42bfdee1e3bca923277fb7fe0e0314a5
sorenlundj <Sorenlundj@gmail.com>
2020-01-26 10:34:29 +0100

    Added funcitonality
    + add_info
    + get_info
    + fixed indexing in add_dependency

--- code/erlang/mmods.erl
+++ code/erlang/mmods.erl
@@ -2,10 +2,12 @@
 
 -behaviour(gen_statem).
 
--export([start/1, add_relation/2, get_state/1, get_type/1, get_relations/1, add_dependency/3]).
+-export([start/1, add_relation/2, add_dependency/3, add_info/2]).
+-export([get_state/1, get_type/1, get_relations/1, get_info/1]).
 
-%-export([init/1, callback_mode/0, terminate/3, code_change/4]).
 -export([init/1, callback_mode/0, terminate/3, code_change/4, mmods_handler/3]).
+
+%-export([insert_index/3]).
 
 -define(SERVER, ?MODULE).
 
@@ -34,6 +36,9 @@ add_relation(From, To) ->
 add_dependency(From, To, Dependency) ->
  gen_statem:call(From, {add_dependency_call, {To, Dependency}}).
 
+add_info(Id, Data) ->
+  gen_statem:call(Id, {add_info_call, Data}).
+
 get_state(Id) ->
   gen_statem:call(Id, {get_state_call, none}).
 
@@ -43,13 +48,16 @@ get_type(Id) ->
 get_relations(Id) ->
   gen_statem:call(Id, {get_relations_call, none}).
 
+get_info(Id) ->
+  gen_statem:call(Id, {get_info_call, none}).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Callback functions                                                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 init(Type)->
-  {ok, mmods_handler, {Type, [], self()}}.
+  {ok, mmods_handler, {Type, [], self(), []}}.
 
 callback_mode() ->
   state_functions.
@@ -65,22 +73,13 @@ code_change(_Vsn, State, Data, _Extra) ->
 % Top-level function                                                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%mmods_handler(cast, {Function_name, Request}, {Type, Relations}=State) ->
-% case Function_name of
-%   add_relation_call ->
-%     {To, Dependency} = Request, 
-%
-%
-% end.
-
-
-mmods_handler({call, From}, {Function_name, Request}, {Type, Relations, Self}=State) ->
+mmods_handler({call, From}, {Function_name, Request}, {Type, Relations, Self, Info}=State) ->
   case Function_name of
     add_relation_call ->
       To = Request,
       case legal_relation(Type, get_type(To)) of
         true  -> 
-          {keep_state, {Type, [{To, []}|Relations], Self}, [{reply, From, {ok, Self}}]};
+          {keep_state, {Type, [{To, []}|Relations], Self, Info}, [{reply, From, {ok, Self}}]};
         false ->
           {keep_state, State, [{reply, From, {error, illegal_relation}}]}
       end;
@@ -88,20 +87,20 @@ mmods_handler({call, From}, {Function_name, Request}, {Type, Relations, Self}=St
       {To, Dependency} = Request,
       case relation_exists(To, Relations) of
         true  ->
-          Updated_relations = dep_to_rel_list(Dependency, Relations),
-          {keep_state, {Type, Updated_relations, Self}, [{reply, From, {ok, Dependency}}]};
+          Updated_relations = dep_to_rel_list(To, Dependency, Relations, []),
+          {keep_state, {Type, Updated_relations, Self, Info}, [{reply, From, {ok, Dependency}}]};
         false ->
           {keep_state, State, [{reply, From, {error, relation_doesnt_exist}}]}
       end;
-    get_state_call ->
-      {keep_state, State, [{reply, From, State}]};
-    get_type_call ->
-      {keep_state, State, [{reply, From, Type}]};
-    get_relations_call ->
-      {keep_state, State, [{reply, From, Relations}]};
-    _ ->
-      {keep_state, State, [{reply, From, {error, unhandled_case}}]}
-    end.
+    add_info_call ->
+      New_info = Request,
+        {keep_state, {Type, Relations, Self, [New_info|Info]}, [{reply, From, {ok, Self}}]};
+    get_state_call     -> {keep_state, State, [{reply, From, State}]};
+    get_type_call      -> {keep_state, State, [{reply, From, Type}]};
+    get_relations_call -> {keep_state, State, [{reply, From, Relations}]};
+    get_info_call      -> {keep_state, State, [{reply, From, Info}]};
+    _                  -> {keep_state, State, [{reply, From, {error, unhandled_case}}]}
+  end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Non-op-level functions                                            %
@@ -126,20 +125,23 @@ relation_exists(From, [{R, _}|Relations]) ->
     false -> relation_exists(From, Relations)
   end.
 
-dep_to_rel_list(Dep, [{Rel_head, Dep_list}|Rel_list]) ->
-  [{Rel_head, [Dep|Dep_list]}|Rel_list].
+dep_to_rel_list(_, _, [], _) ->
+  error;
+dep_to_rel_list(To, Dep, [{Rel_head, Dep_list}|Rel_list], Rest_list) ->
+  case To == Rel_head of
+    true  -> Rest_list ++ [{Rel_head, [Dep|Dep_list]}|Rel_list];
+    false -> dep_to_rel_list(To, Dep, Rel_list, Rest_list ++ [{Rel_head, Dep_list}])
+  end.
   
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % TODOS
-% felt i state 'data' (her ligger kortet)
 % funktion 'transfer data', der tjekker om der er en relation, og så gør den bare
+% case Type of
+%        ship -> kan ikke
+%        service -> send data, slet data
+%        company -> send data
+
 % funktion 'request' (ship->service->company), der ser om der er en vej fra ship til company, og så reqer data
-
-% på linje 94 er det Relations, der skal bruges (og ikke get_relations(to))
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
--- code/erlang/tests.erl
+++ code/erlang/tests.erl
@@ -1,6 +1,12 @@
 -module(tests).
 
--import(mmods, [start/1, add_relation/2, get_state/1, get_type/1, get_relations/1, add_dependency/3]).
+-import(mmods, [start/1,
+                add_relation/2,
+                get_state/1,
+                get_type/1,
+                get_relations/1,
+                add_dependency/3,
+                add_info/2]).
 
 -compile(export_all).
 -compile(nowarn_export_all).
@@ -16,6 +22,9 @@ main() ->
   {ok, test1}   = mmods:add_dependency(Ship, Service, test1),
   {ok, test2}   = mmods:add_dependency(Ship, Service, test2),
   {ok, test3}   = mmods:add_dependency(Ship, Service, test3),
+  mmods:add_info(Company, map),
+  mmods:add_info(Company, map),
+  mmods:add_info(Company, map),
   A             = mmods:get_state(Ship),
   B             = mmods:get_state(Service),
   C             = mmods:get_state(Company),
@@ -27,3 +36,21 @@ t2() ->
   A = mmods:get_state(Ship1),
   B = mmods:get_state(Ship2),
   {A, B}.
+
+deps_true() ->
+  {ok, Ship}    = mmods:start(ship),
+  {ok, Service1} = mmods:start(service),
+  {ok, Service2} = mmods:start(service),
+  {ok, Service3} = mmods:start(service),
+  {ok, Service4} = mmods:start(service),
+  {ok, Ship}    = mmods:add_relation(Ship, Service1),
+  {ok, Ship}    = mmods:add_relation(Ship, Service2),
+  {ok, Ship}    = mmods:add_relation(Ship, Service3),
+  {ok, Ship}    = mmods:add_relation(Ship, Service4),
+  {ok, service1}   = mmods:add_dependency(Ship, Service1, service1),
+  {ok, service2}   = mmods:add_dependency(Ship, Service2, service2),
+  {ok, service3}   = mmods:add_dependency(Ship, Service3, service3),
+  {ok, service4}   = mmods:add_dependency(Ship, Service4, service4),
+  mmods:get_state(Ship).
+
+
